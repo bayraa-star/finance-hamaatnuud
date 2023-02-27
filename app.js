aa2d9cff56b37c4c070928686e6ea69e3fd55dc0
bayraa-star <batbayar.tsogbayar.rokda@gmail.com>
2023-02-27 16:39:21 +0800

    Update js
    Finance module ruu item add hiideg bolgoloo

--- app.js
+++ app.js
@@ -22,15 +22,59 @@ var uiController = (function () {
 })();
 
 //Санхүүтэй ажиллах удирдлага
-var financeController = (function () {})();
+//Private function
+var financeController = (function () {
+  var Income = function (id, description, value) {
+    this.id = id;
+    this.decription = description;
+    this.value = value;
+  };
+  //Private function
+  var Expense = function (id, description, value) {
+    this.id = id;
+    this.description = description;
+    this.value = value;
+  };
+
+  //Private data
+  var data = {
+    items: {
+      inc: [],
+      exp: [],
+    },
+  };
+
+  var totals = {
+    inc: 0,
+    exp: 0,
+  };
+  return {
+    addItem: function (type, description, val) {
+      var item, id;
+
+      if (data.items[type].length === 0) id = 1;
+      else {
+        data.items[types][data.items[type].length - 1].id + 1;
+      }
+
+      if (type === "inc") {
+        item = new Income(id, desc, val);
+      } else {
+        item = new Expense(id, desc, val);
+      }
+
+      data.items[type].push(item);
+    },
+  };
+})();
 
 //Программыг холбох удирдлага
 var appController = (function (uiController, financeController) {
   var ctrlAddItem = function () {
     // 1. Оруулах өгөгдлийг дэлгэцээс олж авна.
-    console.log(uiController.getInput());
+    var input = uiController.getInput();
     // 2. Олж авсан өгөгдлүүдээ санхүүгийн удирдлагад дамжуулж, тэнд хадгална.
-    console.log("Олж авсан өгөгдлүүдээ санхүүгийн удирдлагад дамжуулах хэсэг");
+    financeController.addItem();
     // 3. Олж авсан өгөгдлүүдийг веб дээрээ тохирох хэсэгт нь гаргана.
     console.log(
       "Олж авсан өгөгдлүүдийг веб дээрээ тохирох хэсэгт нь гаргах хэсэг"
