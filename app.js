33ae97cd24ee51d4adf792b400c68bdef3f5ff69
bayraa-star <batbayar.tsogbayar.rokda@gmail.com>
2023-03-01 18:43:15 +0800

    Finish
    UI өөрчлөв. Төсөл дууслаа.

--- app.js
+++ app.js
@@ -54,6 +54,23 @@ var uiController = (function () {
         unuudur.getMonth() +
         " сарын Өрхийн төсөв";
     },
+
+    changeType: function () {
+      var fields = document.querySelectorAll(
+        DOMstrings.inputType +
+          ", " +
+          DOMstrings.inputDescription +
+          ", " +
+          DOMstrings.inputValue
+      );
+
+      nodeListForEach(fields, function (el) {
+        el.classList.toggle("red-focus");
+      });
+
+      document.querySelector(DOMstrings.addBtn).classList.toggle("red");
+    },
+
     getInput: function () {
       return {
         type: document.querySelector(DOMstrings.inputType).value,
@@ -320,6 +337,10 @@ var appController = (function (uiController, financeController) {
         ctrlAddItem();
       }
     });
+
+    document
+      .querySelector(DOM.inputType)
+      .addEventListener("change", uiController.changeType);
 
     document
       .querySelector(DOM.containerDiv)
